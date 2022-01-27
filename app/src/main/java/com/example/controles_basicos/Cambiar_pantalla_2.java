f265d2ff4c0fcd7fb2088b223c87794c0d7e430f
Estela25 <USIS035821@ugb.edu.sv>
2022-01-27 18:07:23 -0500

    Diseño de cambiar pantalla

---
+++ app/src/main/java/com/example/controles_basicos/Cambiar_pantalla_2.java
@@ -0,0 +1,14 @@
+package com.example.controles_basicos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cambiar_pantalla_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cambiar_pantalla2);
+    }
+}
