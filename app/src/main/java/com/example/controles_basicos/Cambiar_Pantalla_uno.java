76ebdd976533ddcedfc049be7c5ad4fd86d5496c
JOSÉ LUIS MUÑOZ PADILLA <USIS043721@ugb.edu.sv>
2022-01-27 16:53:53 -0600

    Controles basicos codigo

---
+++ app/src/main/java/com/example/controles_basicos/Cambiar_Pantalla_uno.java
@@ -0,0 +1,14 @@
+package com.example.controles_basicos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cambiar_Pantalla_uno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cambiar_pantalla_uno);
+    }
+}
--- app/src/main/java/com/example/controles_basicos/MainActivity.java
+++ app/src/main/java/com/example/controles_basicos/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.controles_basicos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText tNombre = (EditText)this.findViewById(R.id.etNombre);
+        final EditText tApellido = (EditText)this.findViewById(R.id.etApellido);
+        Button bDatos = (Button)this.findViewById(R.id.btEnviarDatos);
+
+        bDatos.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                String nom = tNombre.getText().toString();
+                String ape = tApellido.getText().toString();
+
+                Toast.makeText(getApplicationContext(), "Tu Nombre es: " +nom+" " +ape,Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
